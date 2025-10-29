a55a27d1d17f45f61da82eb804ef691b967eb8a3
unknown <ivanderdaniel70@gmail.com>
2025-10-29 11:29:28 +0700

    test

--- python/challenges/3_loop_odd_vocal.py
+++ python/challenges/3_loop_odd_vocal.py
@@ -1,11 +1,15 @@
 # TODO: Program untuk mencetak bilangan ganjil dari 1 hingga 15
 odd_numbers = [1,3,5,7,9,11,13,15]
-print(odd_numbers)
+for i in odd_numbers:
+    print(odd_numbers)
 
 # TODO: Program menghitung jumlah huruf vokal
 word = input("Masukkan kata: ").lower()
 
 vowels = "aiueo"
 count = 0
-
+for char in word :
+    if char in vowels:
+        count += 1
+        
 print("Jumlah huruf vokal:", count)
--- python/challenges/4_function_is_even_factorial.py
+++ python/challenges/4_function_is_even_factorial.py
@@ -1,15 +1,32 @@
 # TODO: Fungsi untuk memeriksa apakah sebuah bilangan genap
+num = (4,7)
 def is_even(num):
-    pass
+    if num %2 == 0 :
+        print ('Bilangan Genap')
+    else :
+        print ('Bukan Genap')
+    
 
 # Tes fungsi
-print(is_even(4))   # True
-print(is_even(7))   # False
+is_even(4)   # True
+is_even(7)  # False
 
 # TODO: Fungsi untuk menghitung faktorial
+n = (0,5)
+
 def factorial(n):
-    pass
+    if n == 0:
+        print ('Faktorial : 1')
+    elif n <= 0:
+        print ('Faktorial tidak bisa bilangan negatif')
+    else:
+        a = 1
+        factorial = 1
+        while a <= n :
+            factorial *= a
+            a += 1
+            print (f'faktorial : {factorial}')
 
 # Tes fungsi
-print(factorial(5))  # 120
-print(factorial(0))  # 1
+factorial(5) # 120
+factorial(0) # 1
